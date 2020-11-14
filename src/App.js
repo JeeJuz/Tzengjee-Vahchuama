dae7bf9488d8ca6e21589783b53810a01895d8de
JeeJuz <themsgkid@gmail.com>
2020-11-13 19:38:49 -0600

    porto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react'
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import React from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
+
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+
+
+
+
+
 
 class App extends React.Component {
 
@@ -9,14 +22,14 @@ class App extends React.Component {
     this.state = {
         title: "Tzengjee Vahchuama",
         headerLinks: [
-          {title: "Home", path: '/'},
-          {title: "About", path: '/about'},
+          {title: 'Home', path: '/'},
+          {title: 'About', path: '/about'},
           {title: 'Contact', path: '/contact'},
         ],
       home: {
         title: 'Be Happy',
         subtitle: "Tzengjee's Porfolio",
-        text: "Work in Progress",
+        text: 'Work in Progress',
       },
       about: {
         title: 'About me'
@@ -24,16 +37,37 @@ class App extends React.Component {
       contact: {
         title: 'Let\'s talk'
       }
-        }
     }
   }
 
-  render(){
+  render() {
     return (
       <Router>
+        <Container className="p-0" fluid={true}>
 
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Tzengjee Vahchuama</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+
+          <Route path="/" exact render={() => <HomePage title ={this.state.home.title} subTitle ={this.state.home.subTitle} text ={this.state.home.text} />} />
+          <Route path="/about"  render={() => <AboutPage title ={this.state.about.title} />} />
+          <Route path="/contact"  render={() => <ContactPage title ={this.state.contact.title} />} />
+         
+          <Footer/>
+
+        </Container>
       </Router>
-    )
+    );
   }
 }
 
